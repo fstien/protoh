f5e162f827c644ec60f3fa3e2f6de4b6bb5b4270
Francois Stiennon <francois.stiennon@gmail.com>
2025-11-28 15:21:05 +0000

    more code

--- main.go
+++ main.go
@@ -2,10 +2,12 @@
 package main
 
 import (
-	"encoding/binary"
+	"bufio"
+	"context"
 	"fmt"
-	"io"
 	"net"
+	"strings"
+	"sync"
 )
 
 func main() {
@@ -17,6 +19,8 @@ func main() {
 
 	fmt.Println("listening on 8080")
 
+	b := &messageBroker{}
+
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -24,62 +28,151 @@ func main() {
 			continue
 		}
 
-		go handleConn(conn)
+		go handleConn(b, conn)
 	}
 }
 
-func handleConn(conn net.Conn) {
-	defer conn.Close()
+type messageBroker struct {
+	subscribers map[string]chan message
+	mu          sync.Mutex
+}
 
-	prices := make(map[int32]int32)
-	buf := make([]byte, 9)
+func (b *messageBroker) send(msg message) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
 
-	for {
-		n, err := io.ReadFull(conn, buf)
-		if err != nil || n != 9 {
-			fmt.Printf("read error: %s, n: %d", err.Error(), n)
-			return
-		}
-
-		i1 := int32(binary.BigEndian.Uint32(buf[1:5]))
-		i2 := int32(binary.BigEndian.Uint32(buf[5:9]))
-
-		switch buf[0] {
-		case 'I':
-			ts := i1
-			price := i2
-
-			prices[ts] = price
-		case 'Q':
-			mintime := i1
-			maxtime := i2
-
-			sum := 0
-			c := 0
-
-			for ts, p := range prices {
-				if ts >= mintime && ts <= maxtime {
-					sum += int(p)
-					c++
-				}
-			}
-
-			mean := 0
-			if c > 0 {
-				mean = sum / c
-			}
-
-			bytes := make([]byte, 4)
-			binary.BigEndian.PutUint32(bytes, uint32(mean))
-
-			n, err := conn.Write(bytes)
-			if err != nil || n != 4 {
-				fmt.Printf("failed to respond with mean %s, %d", err.Error(), n)
-				return
-			}
-		default:
-			fmt.Println("invalid request: ", buf[0])
-			return
+	for name, c := range b.subscribers {
+		if name != msg.sender {
+			c <- msg
 		}
 	}
+}
+
+func (b *messageBroker) sub(name string) (chan message, error) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	if b.subscribers[name] != nil {
+		return nil, fmt.Errorf("subscriber already exists with name %s", name)
+	}
+
+	for _, s := range b.subscribers {
+		s <- message{content: fmt.Sprintf("%s has joined the room", name)}
+	}
+
+	c := make(chan message, 1)
+	b.subscribers[name] = c
+	return c, nil
+}
+
+func (b *messageBroker) members() []string {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	m := []string{}
+
+	for s, _ := range b.subscribers {
+		m = append(m, s)
+	}
+
+	return m
+}
+
+func (b *messageBroker) unsub(name string) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	for _, s := range b.subscribers {
+		s <- message{content: fmt.Sprintf("%s has left the room", name)}
+	}
+
+	delete(b.subscribers, name)
+}
+
+type message struct {
+	sender  string
+	content string
+}
+
+func handleConn(b *messageBroker, conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	_, err := conn.Write([]byte("Welcome to budgetchat! What shall I call you?\n"))
+	if err != nil {
+		fmt.Println("failed to write greeting", err)
+		return
+	}
+
+	name, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Printf("read error: %s", err.Error())
+		return
+	}
+
+	if len(name) < 1 || len(name) > 1000 || !IsASCIIAlphanumeric(name) {
+		conn.Write([]byte("invalid name"))
+		return
+	}
+
+	_, err = conn.Write([]byte(fmt.Sprintf("* The room contains: %s", strings.Join(b.members(), ", "))))
+	if err != nil {
+		fmt.Println("failed to write members", err)
+	}
+
+	c, err := b.sub(name)
+	if err != nil {
+		fmt.Println("failed to subscribe", err)
+		return
+	}
+	defer b.unsub(name)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func(cancel context.CancelFunc, b *messageBroker, r *bufio.Reader) {
+		for {
+			msg, err := reader.ReadString('\n')
+			if err != nil {
+				cancel()
+				return
+			}
+			b.send(message{
+				sender:  name,
+				content: msg,
+			})
+		}
+	}(cancel, b, reader)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case msg := <-c:
+			if msg.sender == "" {
+				_, err = conn.Write([]byte(fmt.Sprintf("* %s", msg.content)))
+				if err != nil {
+					fmt.Println("failed to write msg", err)
+				}
+			} else {
+				_, err = conn.Write([]byte(fmt.Sprintf("[%s] %s", msg.sender, msg.content)))
+				if err != nil {
+					fmt.Println("failed to write msg", err)
+				}
+			}
+		}
+	}
+
+}
+
+func IsASCIIAlphanumeric(s string) bool {
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if !((c >= 'A' && c <= 'Z') ||
+			(c >= 'a' && c <= 'z') ||
+			(c >= '0' && c <= '9')) {
+			return false
+		}
+	}
+	return true
 }
